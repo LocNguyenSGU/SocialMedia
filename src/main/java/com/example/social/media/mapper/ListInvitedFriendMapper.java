e2a8c978612a6cb6354a6d2090d96d4db11ffbd1
lamkbv <lamkbvn@gmail.com>
2025-03-14 11:36:56 +0700

    no update

--- src/main/java/com/example/social/media/mapper/ListInvitedFriendMapper.java
+++ src/main/java/com/example/social/media/mapper/ListInvitedFriendMapper.java
@@ -4,15 +4,13 @@ import com.example.social.media.entity.ListInvitedFriend;
 import com.example.social.media.payload.request.ListInvitedFriend.ListInvitedFriendCreateRequest;
 import com.example.social.media.payload.request.ListInvitedFriend.ListInvitedFriendUpdateRequest;
 import com.example.social.media.payload.response.ListInvitedFriendDTO.ListInvitedFriendResponseDTO;
-import org.mapstruct.BeanMapping;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
-
 public interface ListInvitedFriendMapper {
     ListInvitedFriend toListInvitedFriend(ListInvitedFriendCreateRequest request);
+
+    @Mapping(ignore = true ,target = "sender")
     ListInvitedFriendResponseDTO toListInvitedFriendResponseDTO(ListInvitedFriend listInvitedFriend);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
