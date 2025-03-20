a94fb5e1e34dc86a48783703273793e4d0a5ec9a
quachhuuvinh <vhuu3208@gmail.com>
2025-03-20 21:47:04 +0700

    feat: admin controller

---
+++ src/main/java/com/example/social/media/payload/response/AdminDTO/RoleDTO.java
@@ -0,0 +1,14 @@
+package com.example.social.media.payload.response.AdminDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RoleDTO {
+    String name;
+    String description;
+
+}
